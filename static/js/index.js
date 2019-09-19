c363a102fd7fb8467b626f09e4a02c69f2f83bd6
Doyin <doyin315@gmail.com>
2019-09-19 16:03:51 +0100

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,20 @@
-s=document.getElementById('status')
-s.innerText='View More'
+content=document.getElementById('more')
+less=document.getElementById('less')
+more=document.getElementById('status')
 
-p=document.getElementById('more')
-p.style.display='none';
-s.addEventListener('click', function(){
-    if (s.innerText=='View More'){
-    s.innerText='View Less';
-    p.style.display='inline';
-    }
-    else{
-        s.innerText='View More'
-        p.style.display='none';
-    }
+content.style.display='none'
+less.style.display='none'
+more.addEventListener('click',function()
+{
+    less.style.display='inline'
+    content.style.display='inline'
+    more.style.display='none'
+})
+
+less.addEventListener('click',function()
+{
+    more.style.display='inline'
+    content.style.display='none'
+    less.style.display='none'
 })
 
