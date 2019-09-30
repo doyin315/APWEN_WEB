e1da8c2025ed7edd95df475540b17723767c6b39
doyin315 <doyin315@gmaiil.com>
2019-09-30 16:28:51 +0100

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,14 @@
 content=document.getElementById('more')
 less=document.getElementById('less')
 more=document.getElementById('status')
-
+info=document.getElementById('more_info')
+read_button=document.getElementById('read_button')
+readless_button=document.getElementById('readless_button')
 content.style.display='none'
 less.style.display='none'
+info.style.display='none'
+readless_button.style.display='none'
+
 more.addEventListener('click',function()
 {
     less.style.display='inline'
@@ -17,4 +22,13 @@ less.addEventListener('click',function()
     content.style.display='none'
     less.style.display='none'
 })
-
+read_button.addEventListener('click', function(){
+    info.style.display='inline' 
+    read_button.style.display='none' 
+    readless_button.style.display='inline'  
+})
+readless_button.addEventListener('click', function(){
+    info.style.display='none'
+    read_button.style.display='inline'
+    readless_button.style.display='none'
+})
